feec9de0de11222edd67c4af2022112380a495de
josh lim <joshlim@joshs-MacBook-Air.local>
2025-01-17 13:25:06 +0900

    view, text, image practice

--- react-native-client/app/(tabs)/about.tsx
+++ react-native-client/app/(tabs)/about.tsx
@@ -1,27 +1,34 @@
-import { Text, View, StyleSheet, Button } from 'react-native';
-
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AboutScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>About Screen</Text>
-            <Button onPress={testFunc} title='Button' color='#ffff'/>
-        </View>
-    );
-}
 
-function testFunc() {
-    console.log("test");
+    const handlePress = () => {
+        console.log("text pressed");
+    }
+    const myText = 'view: most basic building block. can use SafeAreaView to make sure content does not conflict spacially with device displays text: text can have styles that use StyleSheet, images can either bring local image or from an api - local -> requier("route") ; api -> {uri, width, height}'
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text} onPress={handlePress}>What I've learnt</Text>
+            <Text style={styles.text}>{myText}</Text>
+            <Image source={{
+                uri: "https://picsum.photos/200/300",
+                width: 300,
+                height: 300,
+            }} resizeMode='contain' />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#25292e',
+        backgroundColor: '#fff',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     text: {
-        color: '#fff',
+        color: '#000',
     },
 })
